e33e8170179c1a733f59f3bdfe961f2f39ea5468
r.nishino <nishino203xx@gmail.com>
2025-11-30 00:28:48 +0900

    add:UIを配置

--- app/src/features/counter/pages/CounterPage.tsx
+++ app/src/features/counter/pages/CounterPage.tsx
@@ -2,9 +2,14 @@ export default function CounterPage() {
   return (
     <>
       <h1>Auto Counter</h1>
-      <span>ボタン操作と自動カウント機能で数値を増減できる機能。</span>
-
+      <div>ボタン操作と自動カウント機能で数値を増減できる機能。</div>
+      現在のカウント
       <div style={{ fontSize: 100 }}>0</div>
+      <button>+1</button>
+      <button>-1</button>
+      <button>リセット</button>
+      <button>自動カウント開始</button>
+      <div>ステータス：停止中</div>
     </>
   );
 }
