06f6008243017cd498c801b8ccff540d41b05986
r.nishino <nishino203xx@gmail.com>
2025-11-30 02:59:09 +0900

    update:加算、減算、リセット機能を追加

--- app/src/features/counter/pages/CounterPage.tsx
+++ app/src/features/counter/pages/CounterPage.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount((count) => count + 1);
+  const decrement = () => setCount((count) => count - 1);
+  const reset = () => setCount(0);
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>リセット</button>
+      <button>自動カウント開始</button>
+      <div>ステータス：停止中</div>
+    </>
+  );
+};
+
 export default function CounterPage() {
   return (
     <>
       <h1>Auto Counter</h1>
       <div>ボタン操作と自動カウント機能で数値を増減できる機能。</div>
       現在のカウント
-      <div style={{ fontSize: 100 }}>0</div>
-      <button>+1</button>
-      <button>-1</button>
-      <button>リセット</button>
-      <button>自動カウント開始</button>
-      <div>ステータス：停止中</div>
+      <Counter />
     </>
   );
 }
