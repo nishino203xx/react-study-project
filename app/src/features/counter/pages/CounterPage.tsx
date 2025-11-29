7b17226e3816730a7353ddcce1e7208c692b008b
r.nishino <nishino203xx@gmail.com>
2025-11-30 04:35:56 +0900

    update:自動カウント機能を実装

--- app/src/features/counter/pages/CounterPage.tsx
+++ app/src/features/counter/pages/CounterPage.tsx
@@ -1,19 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+  const [autoCount, setAutoCount] = useState(false)
   const increment = () => setCount((count) => count + 1)
   const decrement = () => setCount((count) => count - 1)
   const reset = () => setCount(0)
+  useEffect(() => {
+    if (!autoCount) return
+    const timer = setInterval(() => setCount((count) => count + 1), 1000)
+    return () => clearInterval(timer)
+  }, [autoCount])
   return (
     <>
+      現在のカウント
       <div>{count}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <button onClick={reset}>リセット</button>
-      <button>自動カウント開始</button>
-      <div>ステータス：停止中</div>
+      <AutoCounter
+        autoCount={autoCount}
+        toggle={() => setAutoCount((autoCount) => !autoCount)}
+      />
     </>
+  )
+}
+
+const AutoCounter = ({
+  autoCount,
+  toggle,
+}: {
+  autoCount: boolean
+  toggle: () => void
+}) => {
+  return (
+    <button onClick={toggle}>自動カウント{autoCount ? "停止" : "開始"}</button>
   )
 }
 
@@ -22,7 +43,6 @@ export default function CounterPage() {
     <>
       <h1>Auto Counter</h1>
       <div>ボタン操作と自動カウント機能で数値を増減できる機能。</div>
-      現在のカウント
       <Counter />
     </>
   )
